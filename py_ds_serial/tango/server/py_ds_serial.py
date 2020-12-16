494f3b03abd99615b0a02204d2ba1d122da72761
Alberto López Sánchez <alopez@cells.es>
2020-12-16 11:32:12 +0100

    clarify flush and format

--- py_ds_serial/tango/server/py_ds_serial.py
+++ py_ds_serial/tango/server/py_ds_serial.py
@@ -3,16 +3,13 @@
 # This file is part of the ALBA Python Serial DeviceServer project
 #
 # Copyright (c) 2020 Alberto López Sánchez
-# Distributed under the GNU General Public License v3. See LICENSE for more info.
+# Distributed under the GNU General Public License v3. See LICENSE for more
+# info.
 
 """Tango server class for Py_ds_serial"""
 
-import asyncio
-import urllib.parse
-
-from connio import connection_for_url
 from tango import GreenMode
-from tango.server import Device, attribute, command, device_property
+from tango.server import Device, command, device_property
 
 import py_ds_serial.core
 
@@ -22,42 +19,45 @@ class Py_ds_serial(Device):
     green_mode = GreenMode.Asyncio
 
     # The path and name of the serial line device to be used.
-    serialline = device_property(dtype=str, default_value='/dev/ttyR1')
+    serialline = device_property(dtype=str, doc='')
 
     # The communication speed in baud used with the serial line protocol.
     baudrate = device_property(dtype=int, default_value=9600)
 
-    # The character length used with the serial line protocol. The possibilities are 8, 7, 6 or 5 bits per character.
+    # The character length used with the serial line protocol.
+    # The possibilities are 8, 7, 6 or 5 bits per character.
     charlength = device_property(dtype=int, default_value=8)
 
-    # End of message Character used in particular by the DevSerReadLine command Default = 13
+    # End of message Character used in particular by the DevSerReadLine
+    # command Default = 13
     newline = device_property(dtype=int, default_value=13)
 
-    # The parity used with the serial line protocol. The possibilities are none = empty, even or odd.
+    # The parity used with the serial line protocol. The possibilities
+    # are none = empty, even or odd.
     # TODO: Assert the possibilities.
     parity = device_property(dtype=str, default_value='none')
 
-
-
-    # The timout value im ms for for answers of requests send to the serial line.
-    # This value should be lower than the Tango client server timout value.
+    # The timout value im ms for for answers of requests send to the serial
+    # line. This value should be lower than the Tango client server timout
+    # value.
     timeout = device_property(dtype=int, default_value=100)
 
-    # The number of stop bits used with the serial line protocol. The possibilities are 1 or 2 stop bits.
+    # The number of stop bits used with the serial line protocol. The
+    # possibilities are 1 or 2 stop bits.
     # TODO: Assert the possibilities.
-    stopbits = device_property(dtype=int, default_value=1)
+    stopbits = device_property(dtype=int, default_value=1, doc='asjjssj')
 
-
+    # TODO: Init
     async def init_device(self):
         await super().init_device()
         self.connection = None
         self.py_ds_serial = py_ds_serial.core.Py_ds_serial(self.connection)
 
-
     @command
     def DevSerWriteString(self, string: str) -> int:
         """
-        Write a string of characters to a serial line and return the number of characters written.
+        Write a string of characters to a serial line and return the number of
+        characters written.
         """
         # TODO
         return self.py_ds_serial.write_string(string)
@@ -65,23 +65,18 @@ class Py_ds_serial(Device):
     @command
     def DevSerFlush(self, what: int) -> None:
         """
-        Flush serial line port according to argin passed. 0=input 1=output 2=both.
+        Flush serial line port according to argin passed. 0=input 1=output
+        2=both.
         """
-        # TODO: Comprobar que el comportamiento es el esperado. flush input discards. flush output waits to write
-        assert what in [0, 1, 2]
-        if what == 0:
-            self.py_ds_serial.flush()
-        elif what == 1:
-            self.py_ds_serial.reset_input_buffer()
-        elif what == 2:
-            self.py_ds_serial.flush()
-            self.py_ds_serial.reset_input_buffer()
-
+        # TODO: Comprobar que el comportamiento es el esperado. flush input
+        # discards. flush output waits to write
+        self.py_ds_serial.clear_buff(what)
 
     @command
     def DevSerReadChar(self, argin: int) -> bytes:
         """
-        Read an array of characters, the type of read is specified in the input parameter, it can be SL_RAW SL_NCHAR SL_LINE.
+        Read an array of characters, the type of read is specified in the input
+        parameter, it can be SL_RAW SL_NCHAR SL_LINE.
         """
         # TODO: Check
         # SL_RAW = 0, SL_NCHAR=1, SL_LINE=2
@@ -99,7 +94,8 @@ class Py_ds_serial(Device):
     @command
     def DevSerReadRaw(self) -> bytes:
         """
-        Read a string from the serialline device in mode raw (no end of string expected, just empty the entire serialline receiving buffer).
+        Read a string from the serialline device in mode raw (no end of string
+        expected, just empty the entire serialline receiving buffer).
         """
         # TODO: Check
         return self.py_ds_serial.readall()
@@ -107,7 +103,8 @@ class Py_ds_serial(Device):
     @command
     def DevSerWriteChar(self, bytes) -> int:
         """
-        Write N characters to a seria line and return the number of characters written.
+        Write N characters to a seria line and return the number of characters
+        written.
         """
         # TODO: Check
         return self.py_ds_serial.write_string(bytes)
@@ -115,10 +112,12 @@ class Py_ds_serial(Device):
     @command
     def Init(self) -> None:
         """
-        Reloads the value of the properties and restarts the connection to keep it working.
+        Reloads the value of the properties and restarts the connection to keep
+        it working.
         """
         # TODO: Check
         pass
+
 
 if __name__ == "__main__":
     import logging
