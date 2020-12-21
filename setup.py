54008c10b88411742dcbad874ed9f9624928da6c
Alberto López Sánchez <alopez@cells.es>
2020-12-21 14:10:49 +0100

    update version number

--- setup.py
+++ setup.py
@@ -68,6 +68,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/catunlock/tango_serial',
-    version='0.1.0',
+    version='0.1.1',
     zip_safe=False,
 )
